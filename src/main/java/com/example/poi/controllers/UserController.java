601fb66f495f54d8bab1d28c08fb2492593ba6a2
FAYZAABDELSALAM\FayzaAbdelSalam <fayza.abdelsalam@egabi.com>
2023-02-28 11:24:23 +0200

    28Feb11:25

--- src/main/java/com/example/poi/controllers/UserController.java
+++ src/main/java/com/example/poi/controllers/UserController.java
@@ -1,14 +1,20 @@
 package com.example.poi.controllers;
 
 
+import java.io.ByteArrayOutputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.poi.services.UserService;
-import com.itextpdf.text.DocumentException;
 
 @RestController
 @RequestMapping("/user")
@@ -17,11 +23,24 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+		static final String DOWNLOAD = "attachment; filename=data.xlsx";
+	
 		@GetMapping(path="/writeFile")
-		public void writeData() throws DocumentException
+		public void writeData()
 		{
-			userService.writeData();
-		}
+			userService.writeData();  
+	    } 
+		
+		@GetMapping(path="/download")
+		public ResponseEntity<ByteArrayResource> downloadTemplate() {
+	            ByteArrayOutputStream stream = userService.download();
+	            HttpHeaders header = new HttpHeaders();
+	            header.setContentType(new MediaType("application", "force-download"));
+	            header.set(HttpHeaders.CONTENT_DISPOSITION, DOWNLOAD);
+	            return new ResponseEntity<>(new ByteArrayResource(stream.toByteArray()),
+	                    header, HttpStatus.CREATED);
+	    }
+
 		@PostMapping(path="/createTable")
 		public void readData()
 		{
--- src/main/java/com/example/poi/services/UserService.java
+++ src/main/java/com/example/poi/services/UserService.java
@@ -1,10 +1,9 @@
 package com.example.poi.services;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,128 +21,142 @@ import com.example.poi.entities.UserEntity;
 import com.example.poi.repositories.UserRepository;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
+@Getter
+@Setter
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	static final String OUTPUTFILE = "dataout.xlsx";
 	static final String INPUTFILE = "data.xlsx";
 	static final String PDFFILE = "ConvertedPDF.pdf";
 	static final String SCHEMA = "kyc db";
-	static final String[] COLUMNS = {"ID","Email","Password"};
-	
-	public void writeData() throws DocumentException 
-	{
-				try (XSSFWorkbook workbook = new XSSFWorkbook()) 
-				{
-					XSSFSheet spreadsheet = workbook.createSheet(SCHEMA);
-					XSSFRow row = spreadsheet.createRow(0);
-					setColumnNames(row);
-					copyEntity(spreadsheet);
-					writeFile(workbook);
-					pdfConverter(workbook);
-				}
-				catch (IOException e) {	e.printStackTrace();	}	
-	}
-	
-		public void pdfConverter(XSSFWorkbook workbook) throws FileNotFoundException, DocumentException
-		{
-				XSSFSheet sheet = workbook.getSheetAt(0);
-		        List<String> headerList = getRow(0,sheet);
+	static final String[] COLUMNS = { "ID", "Email", "Password" };
+	static final String EXCEPTION = "***** :: Exception :: ***** {}";
 
-		        Document document = new Document();
-		        PdfWriter.getInstance(document, new FileOutputStream(PDFFILE));
-
-		        document.open();
-		        PdfPTable table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
-		        addPDFData(true, headerList, table);
-		        int rows = sheet.getLastRowNum();
-		        for (int i = 1; i <= rows; i++) {
-		            List<String> rowList = getRow(i,sheet);
-		            addPDFData(false, rowList, table);
-		            document.add(table);
-		            table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
-		        }
-
-		        document.close();
-		    }
-		
-		public List<String> getRow (int index, XSSFSheet sheet) {
-			List<String> list = new ArrayList<>();
-			
-	        for (Cell cell : sheet.getRow(index)) {
-	            switch (cell.getCellType()) {
-	                case Cell.CELL_TYPE_STRING:
-	                    list.add(cell.getStringCellValue());
-	                    break;
-	                case Cell.CELL_TYPE_NUMERIC:
-	                    list.add(String.valueOf(cell.getNumericCellValue()));
-	                    break;
-	                case Cell.CELL_TYPE_BOOLEAN:
-	                    list.add(String.valueOf(cell.getBooleanCellValue()));
-	                    break;
-	                case Cell.CELL_TYPE_FORMULA:
-	                    list.add(cell.getCellFormula());
-	                    break;
-	                default:
-	            }
-	        }
-			
-//			XSSFRow row = sheet.getRow(index); 
-//			list.add(String.valueOf(row.getCell(1).getNumericCellValue()));
-//			list.add(row.getCell(2).getStringCellValue());
-//			list.add(String.valueOf(row.getCell(3).getNumericCellValue()));
-			return list;
-		}
-	
-	    private void addPDFData (boolean isHeader, List<String> headerList, PdfPTable table) {
-	        headerList.stream().forEach(column -> {
-	                    PdfPCell header = new PdfPCell();
-	                    if (isHeader) {
-	                        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-	                        header.setBorderWidth(2);
-	                    }
-	                    header.setPhrase(new Phrase(column));
-	                    table.addCell(header);
-	                });
-	    }
-	
-	public void writeFile(XSSFWorkbook workbook)
-	{
-		try 
-		{
-		FileOutputStream out = new FileOutputStream(new File(OUTPUTFILE));
-		workbook.write(out);
-		out.close();
-		}
-		catch (final Exception e) { log.error("***** :: Exception :: ***** {}", e.getMessage()); }
-	}
-	
-	public void setColumnNames(XSSFRow row)
-	{	
+	private void setColumnNames(XSSFRow row) {
 		XSSFCell cell;
 		for (int i = 0; i < COLUMNS.length; i++) {
-			cell = row.createCell(i+1);
+			cell = row.createCell(i);
 			cell.setCellValue(COLUMNS[i]);
 		}
 	}
-	
-	public void copyEntity(XSSFSheet spreadsheet)
+
+	private void write(XSSFWorkbook workbook, XSSFSheet spreadsheet)
 	{
+		XSSFRow row = spreadsheet.createRow(0);
+		setColumnNames(row);
+		copyEntity(spreadsheet);
+		writeFile(workbook);
+	}
+	
+	public void writeData() {
+		try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+			XSSFSheet spreadsheet = workbook.createSheet(SCHEMA);
+			write(workbook,spreadsheet);
+			pdfConverter(workbook);
+		} catch (final Exception e) {
+			log.error(EXCEPTION, e.getMessage());
+		}
+	}
+
+	private void writeFile(XSSFWorkbook workbook) {
+		try {
+			FileOutputStream out = new FileOutputStream(new File(OUTPUTFILE));
+			workbook.write(out);
+			out.close();
+		} catch (final Exception e) {
+			log.error(EXCEPTION, e.getMessage());
+		}
+	}
+
+	private void pdfConverter(XSSFWorkbook workbook) {
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		Document document = new Document();
+		try {
+			PdfWriter.getInstance(document, new FileOutputStream(PDFFILE));
+			document.open();
+			createPdf(sheet,document);
+			document.close();
+		} catch (final Exception e) {
+			log.error(EXCEPTION, e.getMessage());
+		}
+	}
+	
+	private void createPdf(XSSFSheet sheet, Document document)
+	{
+		List<String> headerList = getRow(0, sheet);
+		PdfPTable table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
+		addPDFData(true, headerList, table);
+		setPdfTable(sheet, document, table);
+	}
+
+	private void setPdfTable(XSSFSheet sheet, Document document, PdfPTable table) {
+		int rows = sheet.getLastRowNum();
+		for (int i = 1; i <= rows; i++) {
+			List<String> rowList = getRow(i, sheet);
+			addPDFData(false, rowList, table);
+			try {
+				document.add(table);
+			} catch (final Exception e) {
+				log.error(EXCEPTION, e.getMessage());
+			}
+			table = new PdfPTable(sheet.getRow(0).getPhysicalNumberOfCells());
+		}
+	}
+
+	private List<String> getRow(int index, XSSFSheet sheet) {
+		List<String> list = new ArrayList<>();
+
+		for (Cell cell : sheet.getRow(index)) {
+			switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_STRING:
+				list.add(cell.getStringCellValue());
+				break;
+			case Cell.CELL_TYPE_NUMERIC:
+				list.add(String.valueOf(cell.getNumericCellValue()));
+				break;
+			case Cell.CELL_TYPE_BOOLEAN:
+				list.add(String.valueOf(cell.getBooleanCellValue()));
+				break;
+			case Cell.CELL_TYPE_FORMULA:
+				list.add(cell.getCellFormula());
+				break;
+			default:
+			}
+		}
+
+		return list;
+	}
+
+	private void addPDFData(boolean isHeader, List<String> headerList, PdfPTable table) {
+		headerList.stream().forEach(column -> {
+			PdfPCell header = new PdfPCell();
+			if (isHeader) {
+				header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+				header.setBorderWidth(2);
+			}
+			header.setPhrase(new Phrase(column));
+			table.addCell(header);
+		});
+	}
+
+	private void copyEntity(XSSFSheet spreadsheet) {
 		List<UserEntity> userEntity = userRepository.findAll();
 		int rowNo = 1;
-		Map<UserEntity,XSSFRow> map = new HashMap<>();
+		Map<UserEntity, XSSFRow> map = new HashMap<>();
 		for (UserEntity u : userEntity) {
 			XSSFRow row = spreadsheet.createRow(rowNo);
 			map.put(u, row);
@@ -151,55 +164,75 @@ public class UserService {
 		}
 		mapEntityToCopy(map);
 	}
-	
-	public void mapEntityToCopy(Map<UserEntity,XSSFRow> map)
-	{
-		for (Map.Entry<UserEntity, XSSFRow> entry : map.entrySet()) { //4
+
+	private void mapEntityToCopy(Map<UserEntity, XSSFRow> map) {
+		for (Map.Entry<UserEntity, XSSFRow> entry : map.entrySet()) {
 			UserEntity key = entry.getKey();
 			XSSFRow val = entry.getValue();
-			XSSFCell cell = val.createCell(1);
-			cell.setCellValue(key.getId());
-			cell = val.createCell(2);
-			cell.setCellValue(key.getEmail());
-			cell = val.createCell(3);
-			cell.setCellValue(key.getPassword());
+			copyCells(key,val);
 		}
 	}
 	
-	public void saveEntity(XSSFSheet sheet)
+	private void copyCells(UserEntity key, XSSFRow val)
 	{
+		XSSFCell cell = val.createCell(0);
+		cell.setCellValue(key.getId());
+		cell = val.createCell(1);
+		cell.setCellValue(key.getEmail());
+		cell = val.createCell(2);
+		cell.setCellValue(key.getPassword());
+	}
+
+	private void saveEntity(XSSFSheet sheet) {
 		int rows = sheet.getLastRowNum();
-		Map<XSSFRow,UserEntity> map = new HashMap<>();
-		for (int r = 1; r <= rows; r++)
-		{
-			XSSFRow row = sheet.getRow(r);	
+		Map<XSSFRow, UserEntity> map = new HashMap<>();
+		for (int r = 1; r <= rows; r++) {
+			XSSFRow row = sheet.getRow(r);
 			map.put(row, new UserEntity());
 		}
 		mapEntityToSave(map);
 	}
-	
-	public void mapEntityToSave(Map<XSSFRow,UserEntity> map)
-	{
+
+	private void mapEntityToSave(Map<XSSFRow, UserEntity> map) {
 		for (Map.Entry<XSSFRow, UserEntity> entry : map.entrySet()) {
 			XSSFRow key = entry.getKey();
 			UserEntity val = entry.getValue();
-			val.setId(Integer.valueOf((int)key.getCell(0).getNumericCellValue()));
-			val.setEmail(key.getCell(1).getStringCellValue());
-			val.setPassword(String.valueOf(key.getCell(2).getNumericCellValue()));
+			saveCells(key,val);
 			userRepository.save(val);
 		}
 	}
-	
-	public void readData() 
+
+	private void saveCells(XSSFRow key, UserEntity val)
 	{
-		try {
-				try (FileInputStream file = new FileInputStream(new File(INPUTFILE))) 
-				{
-					XSSFWorkbook workbook = new XSSFWorkbook(file);
-					XSSFSheet sheet = workbook.getSheetAt(0);
-					saveEntity(sheet);
-					workbook.close();
-				}
-			} 	catch (final Exception e) { log.error("***** :: Exception :: ***** {}", e.getMessage()); }
+		val.setId(Integer.valueOf((int) key.getCell(0).getNumericCellValue()));
+		val.setEmail(key.getCell(1).getStringCellValue());
+		val.setPassword(String.valueOf(key.getCell(2).getNumericCellValue()));
 	}
+	
+	public void readData() {
+		try {
+			try (FileInputStream file = new FileInputStream(new File(INPUTFILE))) {
+				XSSFWorkbook workbook = new XSSFWorkbook(file);
+				XSSFSheet sheet = workbook.getSheetAt(0);
+				saveEntity(sheet);
+				workbook.close();
+			}
+		} catch (final Exception e) {
+			log.error(EXCEPTION, e.getMessage());
+		}
+	}
+
+	public ByteArrayOutputStream download() {
+		try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+			XSSFSheet spreadsheet = workbook.createSheet(SCHEMA);
+			write(workbook,spreadsheet);
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			workbook.write(stream);
+			return stream;
+		} catch (final Exception e) {
+			log.error(EXCEPTION, e.getMessage());
+			return null;
+		}
+	}
+
 }
